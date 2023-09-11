a82eb1986d99af8e4d790b97aadd6f8bd022a39f
Cam <farrcameron@gmail.com>
2023-09-11 15:56:59 -0700

    update

--- public/index.js
+++ public/index.js
@@ -5,14 +5,28 @@ async function main() {
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
     
-    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=fa0d69473c0e44248ea52651741c9909')
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=fa0d69473c0e44248ea52651741c9909')
     
-    let txt = response.json()
+    let txt = await response.json()
     console.log(txt)
 
     const { GME, MSFT, DIS, BNTX } = mockData;
 
     const stocks = [GME, MSFT, DIS, BNTX];
 }
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor:  'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)'
+        }]
+    }
+});
+
 
 main()
